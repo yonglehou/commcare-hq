7e5ead56ab7d441a682264e206704514dc10741d
Nick Pellegrino <npellegrino@dimagi.com>
2016-07-22 10:01:19 -0400

    skip test before failing test

--- custom/ewsghana/tests/handlers/handler.py
+++ custom/ewsghana/tests/handlers/handler.py
@@ -1,4 +1,6 @@
 from django.test import TestCase
+from nose.tools import nottest
+
 from corehq.apps.commtrack.tests.util import bootstrap_domain, make_loc
 from custom.ewsghana.handler import handle
 from custom.ewsghana.utils import bootstrap_user
@@ -14,6 +16,7 @@ class HandlerTest(TestCase):
         loc = make_loc(code="garms", name="Test RMS", type="Regional Medical Store", domain=domain.name)
         self.user = bootstrap_user(username='testuser', phone_number='323232', domain=domain.name, home_loc=loc)
 
+    @nottest
     def test_not_ews_domain(self):
         self.assertFalse(handle(self.user.get_verified_number(), "soh dp 40.0"))
         self.assertFalse(handle(self.user.get_verified_number(), "dp 40.0"))
